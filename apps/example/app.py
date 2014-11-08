4e78b66e5c68940b6bbb40ef8ce3f269d4189cec
Ashar Rizqi <ashar@apples-mbp.hsd1.ca.comcast.net>
2014-11-08 09:48:33 -0800

    first commit; adding conf dirs, docker file and example app

---
+++ apps/example/app.py
@@ -0,0 +1,10 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route("/hello", methods=["GET"])
+def status():
+  return "Hi!"
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=5000)
